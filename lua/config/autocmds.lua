852420b34c3dbdd828834bec1c20b52f0b806f29
cryptio-kevin <kevin.ngo@cryptio.co>
2025-09-06 12:39:41 -0700

    fix: neo-tree no longer opens by default

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,14 +6,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- Auto-open Neo-tree on startup
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Small delay to ensure everything is loaded
-    vim.defer_fn(function()
-      vim.cmd("Neotree reveal")
-    end, 10)
-  end,
-})
 
 
