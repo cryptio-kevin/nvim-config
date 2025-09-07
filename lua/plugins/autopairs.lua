8961c469de293f69a2375522956882bfa9d7a179
cryptio-kevin <kevin.ngo@cryptio.co>
2025-09-06 18:14:51 -0700

    Enhance Neovim completion and autopairs plugins
    - Configure advanced autopairs functionality
    - Improve completion engine settings and mappings

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -5,6 +5,16 @@ return {
     opts = {
       check_ts = true,
     },
+    config = function(_, opts)
+      local autopairs = require("nvim-autopairs")
+      autopairs.setup(opts)
+      -- Integrate with nvim-cmp confirm to insert pairs like () after function items
+      local ok_cmp, cmp = pcall(require, "cmp")
+      if ok_cmp then
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end
+    end,
   },
 }
 
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -20,9 +20,20 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
+        experimental = { ghost_text = true },
+        completion = {
+          autocomplete = { cmp.TriggerEvent.TextChanged, cmp.TriggerEvent.InsertEnter },
+          keyword_length = 1,
+        },
         mapping = cmp.mapping.preset.insert({
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ select = false })
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
@@ -33,6 +44,19 @@ return {
             elseif luasnip.jumpable(-1) then luasnip.jump(-1)
             else fallback() end
           end, { "i", "s" }),
+          ["<Down>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item() else fallback() end
+          end, { "i", "s" }),
+          ["<Up>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_prev_item() else fallback() end
+          end, { "i", "s" }),
+          ["<C-n>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item() else fallback() end
+          end, { "i", "s" }),
+          ["<C-p>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_prev_item() else fallback() end
+          end, { "i", "s" }),
+          ["<C-e>"] = cmp.mapping.abort(),
         }),
         sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" } }, { { name = "buffer" }, { name = "path" } }),
       })
